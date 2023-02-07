3d610a0d925776e6fa3574d9d422e560224cbc4b
Thomas Gillard <thomas.gillard@hotmail.com>
2023-02-07 11:23:32 +0100

    V1

--- SandwichSpring/src/main/java/be/thomas/sandwichspring/entity/Sandwich.java
+++ SandwichSpring/src/main/java/be/thomas/sandwichspring/entity/Sandwich.java
@@ -1,15 +1,23 @@
 package be.thomas.sandwichspring.entity;
 
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Setter @Getter
 public class Sandwich {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "sandwich_id", nullable = false)
-    private Long sandwich_id;
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String desc;
+    @Column(nullable = false)
+    private double price;
 
 }
---
+++ SandwichSpring/src/main/java/be/thomas/sandwichspring/repository/SandwichRepository.java
@@ -0,0 +1,8 @@
+package be.thomas.sandwichspring.repository;
+
+    import be.thomas.sandwichspring.entity.Sandwich;
+    import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SandwichRepository extends JpaRepository<Sandwich,Long> {
+} {
+}
